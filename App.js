b170af192948002edca6750db6024a52d31f7b63
dynemoonPi <sebahizi250@gmail.com>
2024-06-18 01:38:50 -0800

    Updated APP by creating Home class

--- App.js
+++ App.js
@@ -6,9 +6,21 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Home/>
     </View>
   );
 }
+
+const Home = () =>{
+
+   return (
+          <View>
+
+            <Text> Welcome to my home page</Text>
+          </View>
+   )
+} 
 
 const styles = StyleSheet.create({
   container: {
